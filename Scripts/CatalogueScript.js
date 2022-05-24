ee226b163634aef70427328fae1e018a99496c86
Eduardo Ferreira <64541360+FujiwaraMokou-tan@users.noreply.github.com>
2022-05-24 01:52:01 +0100

    Add files via upload

---
+++ Scripts/CatalogueScript.js
@@ -0,0 +1,17 @@
+function showArtbooks() {
+  document.getElementById("artbookCatalogue").style.display = "block";
+}
+
+function showStories() {
+  document.getElementById("storiesCatalogue").style.display = "block";
+}
+
+function showComics() {
+  document.getElementById("comicsCatalogue").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("artbookCatalogue").style.display = "none";
+  document.getElementById("storiesCatalogue").style.display = "none";
+  document.getElementById("comicsCatalogue").style.display = "none";
+}
