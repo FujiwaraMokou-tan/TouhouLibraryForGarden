77921ec03e35da84af45ac53b203c6ffd03ea2f2
Eduardo Ferreira <64541360+FujiwaraMokou-tan@users.noreply.github.com>
2022-05-31 04:11:12 +0100

    Add files via upload

--- Scripts/CatalogueScript.js
+++ Scripts/CatalogueScript.js
@@ -15,3 +15,15 @@ function closePopup() {
   document.getElementById("storiesCatalogue").style.display = "none";
   document.getElementById("comicsCatalogue").style.display = "none";
 }
+
+var senseSpeed = 5;
+var previousScroll = 0;
+$(window).scroll(function (event) {
+  var scroller = $(this).scrollTop();
+  if (scroller - senseSpeed > previousScroll) {
+    $("div.test").filter(":not(:animated)").slideUp();
+  } else if (scroller + senseSpeed < previousScroll) {
+    $("div.test").filter(":not(:animated)").slideDown();
+  }
+  previousScroll = scroller;
+});
