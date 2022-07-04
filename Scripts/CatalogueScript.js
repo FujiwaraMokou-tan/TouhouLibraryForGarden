cc09a5aa1c10bbf2d2b46bed3fda1bb759a6b2a7
Eduardo Ferreira <64541360+FujiwaraMokou-tan@users.noreply.github.com>
2022-07-04 22:17:34 +0100

    Add files via upload

--- Scripts/CatalogueScript.js
+++ Scripts/CatalogueScript.js
@@ -1,26 +1,36 @@
 function showArtbooks() {
-  document.getElementById("artbookCatalogue").style.display = "block";
+  document.getElementById("artbookCatalogue").style.display = "flex";
 }
 
 function showStories() {
-  document.getElementById("storiesCatalogue").style.display = "block";
+  document.getElementById("storiesCatalogue").style.display = "flex";
 }
 
 function showComics() {
-  document.getElementById("comicsCatalogue").style.display = "block";
+  document.getElementById("comicsCatalogue").style.display = "flex";
+}
+
+function showInterviews() {
+  document.getElementById("interviewCatalogue").style.display = "flex";
+}
+
+function showCDs() {
+  document.getElementById("cdCatalogue").style.display = "flex";
 }
 
 function closePopup() {
   document.getElementById("artbookCatalogue").style.display = "none";
   document.getElementById("storiesCatalogue").style.display = "none";
   document.getElementById("comicsCatalogue").style.display = "none";
+  document.getElementById("interviewCatalogue").style.display = "none";
+  document.getElementById("cdCatalogue").style.display = "none";
 }
 
 var senseSpeed = 5;
 var previousScroll = 0;
 $("div.indexButton").hide();
 $(window).scroll(function (event) {
-  if ($(this).width() < 522) {
+  if ($(this).width() < 769) {
     var scroller = $(this).scrollTop();
     if (scroller - senseSpeed > previousScroll) {
       $("div.indexButton").filter(":not(:animated)").slideUp();
